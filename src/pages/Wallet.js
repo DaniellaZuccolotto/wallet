eea6e8a5d64cc29f2399a62d481ce45fc84eb694
Daniella Zuccolotto <danielaz_9@hotmail.com>
2022-05-19 10:18:41 -0300

    Fiz o requisito 7

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -140,6 +140,17 @@ class Wallet extends React.Component {
             Adicionar despesa
           </button>
         </form>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
       </main>
 
     );
